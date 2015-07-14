6fe14a6a5f0a5a63aed07d62ab45d1c6f833e299
wxf <wongxiaofeng1024@gmail.com>
2015-07-14 18:07:21 +0800

    beer 第二版, 注意javac编译时在classpath后加入依赖包的本地地址

--- beerV1/src/com/beer/model/BeerExpert.java
+++ beerV1/src/com/beer/model/BeerExpert.java
@@ -1,3 +1,19 @@
+package com.beer.model;
+
+import java.util.*;
+
 public class BeerExpert{
 	
+	public List getBrands(String color){
+		List brand = new ArrayList();
+		if (color.equals("amber")) {
+			brand.add("Jack Amber");
+			brand.add("Red Moose");
+		} else {
+			brand.add("Jail Pale Ale");
+			brand.add("Gout Stout");
+		}
+
+		return brand;
+	}
 }
--- beerV1/src/com/beer/web/BeerSelect.java
+++ beerV1/src/com/beer/web/BeerSelect.java
@@ -1,3 +1,27 @@
-public class BeerSelect {
+package com.beer.web;
+
+
+import javax.servlet.*;
+import java.io.*;
+import javax.servlet.http.*;
+import java.util.*;
+import com.beer.model.BeerExpert;
+
+public class BeerSelect extends HttpServlet{
 	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+		BeerExpert be= new BeerExpert();
+
+		pw.println("Beer selection advice<br/>");
+		String c = req.getParameter("color");
+		List result = be.getBrands(c);
+
+		Iterator it = result.iterator();
+		while(it.hasNext()){
+			pw.println("<br/> tyr: " + it.next());
+		}
+		
+	}
 }
